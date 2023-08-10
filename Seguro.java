427753040f7edfc9ed43371fdfddb1027dd2ae7d
Caio Ramalho <103006310+RamalhoCaio@users.noreply.github.com>
2023-08-10 08:50:43 -0300

    Add files via upload

---
+++ Seguro.java
@@ -0,0 +1,8 @@
+public class Seguro {
+    private float valor;
+    private boolean vigencia;
+
+    private Veiculo veiculo;
+    private Cliente propietario;
+    private Cliente[] assegurados;
+}
---
+++ Veiculo.java
@@ -0,0 +1,9 @@
+public class{
+    private int ano;
+    private String marca;
+    private String cor;
+    private String placa;
+
+    private Modelo modelo;
+    private Seguro seguro;
+}
